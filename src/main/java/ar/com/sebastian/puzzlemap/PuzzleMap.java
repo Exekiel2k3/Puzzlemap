43fa5cd6ecb2c452a7efee058f468ad9e9abecb2
Exekiel2k3 <sebastian.scardigno@gmail.com>
2019-02-03 20:15:23 -0300

    Se quito codigo obsoleto y se realizaron mas casos de pruebas

--- src/main/java/ar/com/sebastian/puzzlemap/PuzzleMap.java
+++ src/main/java/ar/com/sebastian/puzzlemap/PuzzleMap.java
@@ -1,11 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package ar.com.sebastian.puzzlemap;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -13,41 +14,49 @@ import java.util.Random;
  * @author sebas
  */
 public class PuzzleMap {
-
+    
     private Random random = new Random(System.currentTimeMillis());
-    private ArrayList<String> codes = new ArrayList<String>();
+    private HashSet<String> codes = new HashSet<String>(500, (float) 0.9);
     
     private int rows;
     private int columns;
     private int lengthCode;
-
+    private ArrayList<ArrayList<String>> map;
+    
     public PuzzleMap() {
         this.rows = 0;
         this.columns = 0;
-	this.lengthCode = 0;
+        this.lengthCode = 0;
+        this.map = null;
     }
     
     public PuzzleMap(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
+        this.lengthCode = 0;
+        this.map = null;
     }
-
+    
     public int getColumns() {
-        return columns;
+        return this.columns;
     }
-
+    
     public int getRows() {
-        return rows;
+        return this.rows;
     }
-	
+    
     public int getLengthCode() {
-        return lengthCode;
+        return this.lengthCode;
     }
-
+    
+    public ArrayList<ArrayList<String>> getMap() {
+        return this.map;
+    }
+    
     public void setRows(int rows) {
         this.rows = rows;
     }
-
+    
     public void setColumns(int columns) {
         this.columns = columns;
     }
@@ -55,80 +64,56 @@ public class PuzzleMap {
     public void setLengthCode(int lengthCode) {
         this.lengthCode = lengthCode;
     }
-       
-    private String generateNewCode(){
-
-        if(this.lengthCode==0){
+    
+    private String generateNewCode() {
+        
+        if (this.lengthCode == 0) {
             return "";
         }
         
         String code;
         do {
             code = "";
-            for(int i=0; i<this.lengthCode; i++){
+            for (int i = 0; i < this.lengthCode; i++) {
                 int auxRand = random.nextInt(25);
-                char aux[] = Character.toChars( 65 + auxRand);
+                char aux[] = Character.toChars(65 + auxRand);
                 code = code.concat(new String(aux));
             }
-			
-        } while (codes.contains(code));
-        codes.add(code);
+            
+        } while (!codes.add(code));
+        
         return code;
     }
     
-    public ArrayList<ArrayList<String>> create(){
-        
-        ArrayList<ArrayList<String>> outer = new ArrayList<>();
+    public void create() {
+        ArrayList<ArrayList<String>> auxMap = new ArrayList<>();
         ArrayList<String> inner = new ArrayList<>();
         
-        for(int i=0; i<rows; i++){//Rows
-            for(int j=0; j<columns; j++){//Columns
-		inner.add(j, generateNewCode());
+        for (int i = 0; i < rows; i++) {//Rows
+            for (int j = 0; j < columns; j++) {//Columns
+                inner.add(j, generateNewCode());
             }
-            outer.add(i, new ArrayList<>(inner));
+            auxMap.add(i, new ArrayList<>(inner));
             inner.clear();
         }
-        
-        return outer;        
+        map = auxMap;
     }
-	
-//	@Override
-//	public String toString(){
-//		
-//		String map = "";
-//		
-//		for(int i=0; i<rows; i++){//Rows
-//            for(int j=0; j<columns; j++){//Columns
-//				inner.add(j, generateNewCode2());
-//            }
-//            outer.add(i, new ArrayList<>(inner));
-//            inner.clear();
-//        }
-//		
-//		return 	"rows: " + rows + "\n" +
-//				"columns: " + columns "\n" +
-//				"lengthCode: " + lengthCode + "\n" +
-//				
-//	
-//	}
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-               
-//        PuzzleMap puzzleMap = new PuzzleMap(28,36);
-//        puzzleMap.setLengthCode(2);
-//        
-//        for(int i=0; i<puzzleMap.rows; i++){//Rows
-//            for(int j=0; j<puzzleMap.columns; j++){//Columns
-//
-//                System.out.print( puzzleMap.generateNewCode2() + "|");
-//            }
-//            System.out.println();
-//        }
-    }
-
+    @Override
+    public String toString() {
         
+        String auxMap = "";
+        
+        for (int i = 0; i < this.rows; i++) {//Rows
+            for (int j = 0; j < this.columns; j++) {//Columns
+                auxMap = auxMap.concat(map.get(i).get(j)).concat(" ");
+            }
+            auxMap = auxMap.concat("\n");
+        }
+        
+        return "rows: " + rows + "\n"
+                + "columns: " + columns + "\n"
+                + "lengthCode: " + lengthCode + "\n"
+                + "map: " + "\n" + auxMap;
+    }  
 }
--- src/test/java/ar/com/sebastian/puzzlemaptest/PuzzleMapTest.java
+++ src/test/java/ar/com/sebastian/puzzlemaptest/PuzzleMapTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ar.com.sebastian.maparompecabezas;
+package ar.com.sebastian.puzzlemaptest;
 
 import ar.com.sebastian.puzzlemap.PuzzleMap;
 import java.util.ArrayList;
@@ -18,30 +18,32 @@ import org.junit.Before;
 public class PuzzleMapTest {
 
     private PuzzleMap pm1, pm2, pm3;
-    
+
     @Before
-    public void initialize(){
+    public void initialize() {
         pm1 = new PuzzleMap();
         pm2 = new PuzzleMap(30, 20);
         pm3 = new PuzzleMap(2, 3);
         pm3.setLengthCode(2);
     }
-    
+
     public PuzzleMapTest() {
     }
-    
+
     @Test
-    public void testPuzzleMapContructor1(){
-        assertTrue( (pm1.getColumns()==0) && 
-                    (pm1.getRows()==0) && 
-                    (pm1.getLengthCode()==0));
+    public void testPuzzleMapContructor1() {
+        assertTrue((pm1.getColumns() == 0)
+                && (pm1.getRows() == 0)
+                && (pm1.getLengthCode() == 0)
+                && (pm1.getMap() == null));
     }
-    
+
     @Test
-    public void testPuzzleMapContructor2(){
-        assertTrue( (pm2.getColumns()==20) && 
-                    (pm2.getRows()==30) &&
-                    (pm2.getLengthCode()==0));
+    public void testPuzzleMapContructor2() {
+        assertTrue((pm2.getColumns() == 20)
+                && (pm2.getRows() == 30)
+                && (pm2.getLengthCode() == 0)
+                && (pm2.getMap() == null));
     }
 
     @Test
@@ -49,40 +51,72 @@ public class PuzzleMapTest {
         pm1.setRows(13);
         assertEquals(null, 13, pm1.getRows());
     }
-    
+
     @Test
     public void testSetColumns() {
         pm1.setColumns(33);
         assertEquals(null, 33, pm1.getColumns());
-    }    
-	
+    }
+
     @Test
     public void testGetRows() {
         assertEquals(null, 30, pm2.getRows());
     }
-    
+
     @Test
     public void testGetColumns() {
         assertEquals(null, 20, pm2.getColumns());
     }
-    
+
     @Test
     public void testSetLengthCode() {
         pm1.setLengthCode(2);
         assertEquals(null, 2, pm1.getLengthCode());
-    }    
-	
+    }
+
     @Test
     public void testGetLengthCode() {
         assertEquals(null, 0, pm2.getLengthCode());
     }
-	
+
     @Test
-    public void testCreateMap(){
-        ArrayList<ArrayList<String>> map = pm3.create();
-        assertTrue(map.size()==2);
+    public void testGetMap() {
+        pm3.create();
+        ArrayList<ArrayList<String>> map = pm3.getMap();
+        assertTrue(map.size() == 2);
         for (int i = 0; i < pm3.getRows(); i++) {
-            assertTrue(map.get(i).size()==pm3.getColumns());
+            assertTrue(map.get(i).size() == pm3.getColumns());
         }
+    }
+
+    @Test
+    public void testUniqueValue() {
+        ArrayList<ArrayList<String>> map;
+        pm3.create();
+        map = pm3.getMap();
+        int count;
+        
+        for (int i = 0; i < pm3.getRows(); i++) {//Rows
+            for (int j = 0; j < pm3.getColumns(); j++) {//Columns
+                String code = map.get(i).get(j);
+                count = 0;
+                for (int k = 0; k < pm3.getRows(); k++) {//Rows
+                    for (int l = 0; l < pm3.getColumns(); l++) {//Columns
+                        if(code==map.get(k).get(l)){
+                            count++;
+                        }
+                    }
+                }
+                
+                assertTrue(count==1);
+            }
+        }
+    }
+    
+    @Test
+    public void testToString(){
+        pm3.create();
+        System.out.println(pm3.toString());;
+        assertTrue(true);
     }
 }
